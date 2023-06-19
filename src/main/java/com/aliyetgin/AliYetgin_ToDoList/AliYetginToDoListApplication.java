83319d37f377c48419312f0b00159628eb0d5550
aliyetgin <yetgin.ali@outlook.com>
2023-06-19 18:01:30 +0300

    Initial commit

---
+++ src/main/java/com/aliyetgin/AliYetgin_ToDoList/AliYetginToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.aliyetgin.AliYetgin_ToDoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliYetginToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliYetginToDoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aliyetgin/AliYetgin_ToDoList/AliYetginToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aliyetgin.AliYetgin_ToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliYetginToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
